3a92193e9654c221a6956ea7cd7a420c189c80f6
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2016-09-12 20:09:24 -0400

    First Commit

---
+++ test/bookofcalmquotes/core_test.clj
@@ -0,0 +1,7 @@
+(ns bookofcalmquotes.core-test
+  (:require [clojure.test :refer :all]
+            [bookofcalmquotes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
